00ea968387fd46d557456ef1cf918d5932b37c0c
balasachin <38652822+balasachin@users.noreply.github.com>
2018-08-04 07:14:06 +0000

    Create median.c

---
+++ GUVI/median.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+int x[100],n,i;
+int median(int,int[]);
+scanf("%d",&n);
+for(i=0;i<n;i++)
+    scanf("%d",&x[i]);
+printf("median=%d\n",median(n,x));
+getch();
+}
+
+
+
+int median(int n, int x[]) {
+    int temp;
+    int i, j;
+    for(i=0; i<n-1; i++) {
+        for(j=i+1; j<n; j++) {
+            if(x[j] < x[i]) {
+                // swap elements
+                temp = x[i];
+                x[i] = x[j];
+                x[j] = temp;
+            }
+        }
+    }
+
+    if(n%2==0) {
+        return((x[n/2] + x[n/2 - 1]) / 2.0);
+    } else {
+        return x[n/2];
+    }
+}
+  
